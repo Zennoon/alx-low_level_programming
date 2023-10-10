a56cc33071b929c2a3006f1d296c281caea1d4d8
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-10-10 16:13:58 +0000

    Create the proper _putchar function and use it to print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - The entry point of the program
  *
+ * Description: The function uses _putchar function from the header file
+ * to print the word "_putchar"
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fwrite("_putchar\n", 1, 9, stdout);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
