fdadbfd56c1e80885d7fbe5a522181c27d9c96e5
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-10-10 15:09:12 +0000

    Create all initial files and a file as an answer for task 0: '_putchar'

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fwrite("_putchar\n", 1, 9, stdout);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This directory was created to hold my answers / submissions for the project
+0x02. C - Functions, nested loops.
