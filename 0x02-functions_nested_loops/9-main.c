48e61cbd6adb34b0221d62555242106150478c99
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-10-10 21:03:20 +0000

    Create C source files as an answer for task 9: 'Learn your times table'

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - The entry point of the program
+ *
+ * Description: The function uses times_table to printa 9 times table
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
