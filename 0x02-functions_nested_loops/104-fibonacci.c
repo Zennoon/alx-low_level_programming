db50e3706869364a8c09cd80afb29c633e7c4112
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-10-10 22:57:30 +0000

    Create a C source file as an answer for task 16: 'In computer class,...'

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void print_98_fibonacci(void);
+
+/**
+ * main - The entry point of the program
+ *
+ * Description: Uses the print_98_fibonacci function to print the fibonacci series
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_98_fibonacci();
+	return (0);
+}
+
+
+/**
+ * print_fibonacci - Prints the first 98 elements of the fibonacci series
+ *
+ * Description: The function uses a loop, and makes the variables unsigned
+ * because the space is needed for large numbers
+ * Return: void
+ */
+void print_98_fibonacci(void)
+{
+	unsigned long num1 = 0;
+	unsigned long num2 = 1;
+	unsigned long sum = 0;
+	short cnt = 0;
+
+	while (cnt < 97)
+	{
+		sum = num1 + num2;
+		printf("%lu, ", sum);
+		num1 = num2;
+		num2 = sum;
+		cnt++;
+	}
+	printf("%lu\n", num1 + num2);
+}
