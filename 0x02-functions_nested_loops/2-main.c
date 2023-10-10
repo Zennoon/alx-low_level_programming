bd3e9612555b3c09db9f92284056d8e7f3dc939f
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-10-10 17:03:53 +0000

    Create C source files as an answer for task 2: '10 x alphabet'

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - The entry point of the program
+ *
+ * Description: The function uses print_alphabet_10x which in turn uses _putchar.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
