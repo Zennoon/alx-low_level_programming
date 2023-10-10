2e65b979dbfe621caccd0c32ef499bb37a4fdcb0
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-10-10 20:12:01 +0000

    Create C source files as an answer for task 8: 'I'm federal agent Jack Bauer...'

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - The entry point of the program
+ *
+ * Description: The function prints the minutes in 24 hours
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
