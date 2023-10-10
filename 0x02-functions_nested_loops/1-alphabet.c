3fb84d7bc9f6a51569a6f8a68fa3948ccf41c5fa
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-10-10 16:22:57 +0000

    Create C source files as an answer for task 1: 'I sometimes suffer ...'

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - The entry point of the program
+ *
+ * Description: The function uses the print_alphabet function from the 1-main.c
+ * file which in turn uses the _putchar function.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
