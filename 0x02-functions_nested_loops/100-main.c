03e309d76e06fc9fc8403493635b48851a9b4239
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-10-10 21:56:31 +0000

    Create C source files as an answer for task 12: 'The World looks like a ...'

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - The entry point of the program
+ *
+ * Description: The function prints a times table of given size using
+ * print_times_table
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_times_table(15);
+	print_times_table(3);
+	print_times_table(6);
+	return (0);
+}
