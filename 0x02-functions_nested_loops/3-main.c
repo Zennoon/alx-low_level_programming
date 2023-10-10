c5d987f524f89723baa236c4d688ba34436c5f43
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-10-10 18:34:49 +0000

    Create C source files as an answer for task 3: 'islower'

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - The entry point of the function
+ *
+ * Description: The function uses _islower function to check whether some
+ * character is lowercase or not
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r);
+	return (0);
+}
