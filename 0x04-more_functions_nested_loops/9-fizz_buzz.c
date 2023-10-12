a1976f3bfa47978f8aaed45473d413ff933a6b9e
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-10-12 15:01:35 +0000

    Change the spacing between the numbers/words to be as required

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,23 +17,27 @@ int main(void)
 	{
 		if (!(i % 15))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (!(i % 3))
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (!(i % 5))
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
 		if (i == 100)
 		{
 			printf("\n");
+		}
+		else
+		{
+			printf(" ");
 		}
 		i++;
 	}
