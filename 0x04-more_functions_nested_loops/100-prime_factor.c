211036315f0f75beb0adf30165af03f53fdbb49a
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-10-14 08:47:47 +0000

    Change the function so it decrements at a higher rate becoming faster

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+long long unsigned int find_largest_pfactor(void);
+/**
+ * main - The entry point of the program
+ *
+ * Description: Finds an prints the largest prime factor of 612852475143
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%llu\n", find_largest_pfactor());
+	return (0);
+}
+
+/**
+ * find_largest_pfactor - Finds and prints the largest prime factor of the num
+ *
+ * Description: Initially, this function was coded to decrement tmp one number
+ * at a time, but then I noticed that each time tmp was divisible (not prime),
+ * the next viable option was tmp / tmp2, not tmp--. So now, it is much faster.
+ * Return: The largest prime factor of 612852475143
+ */
+long long unsigned int find_largest_pfactor(void)
+{
+	long long unsigned num = 612852475143;
+	long long unsigned tmp = num;
+
+	while (tmp >= 2)
+	{
+		if (!(num % tmp))
+		{
+			short is_prime = 1;
+			long long unsigned tmp2 = 2;
+
+			while (tmp2 < tmp)
+			{
+				if (!(tmp % tmp2))
+				{
+					is_prime = 0;
+					tmp = tmp / tmp2;
+					break;
+				}
+				tmp2++;
+			}
+			if (is_prime)
+			{
+				return (tmp);
+			}
+		}
+	}
+}
