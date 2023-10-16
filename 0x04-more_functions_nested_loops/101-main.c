110720d368b17fffad10f4fe5b0545d3ec277b7d
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-10-16 14:59:51 +0000

    Create initial required files and directories for the project: 'C - Pointers, arrays and strings'

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -17,5 +17,15 @@ int main(void)
     _putchar('\n');
     print_number(-98);
     _putchar('\n');
+    print_number(-0);
+    _putchar('\n');
+    print_number(1232402);
+    _putchar('\n');
+    print_number(-1024);
+    _putchar('\n');
+    print_number(210);
+    _putchar('\n');
+    print_number(-90008);
+    _putchar('\n');
     return (0);
 }
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This directory was created in Oct 16, 2023 to hold my submissions for the
+project '0x05. C - Pointers, arrays and strings'.
