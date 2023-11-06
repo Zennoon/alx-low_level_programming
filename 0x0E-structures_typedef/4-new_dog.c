c3d7ca23afe8224d55ad802461302c6d3137fb81
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-11-06 20:59:53 +0000

    Make a copy of the argument strings using dynamic memory allocation (malloc)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,6 +4,35 @@
 #include "dog.h"
 
 /**
+ * _strlen - Returns the length of a given string
+ * @s: The string whose length is checked
+ *
+ * Return: The length of s
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len))
+	{
+		len++;
+	}
+	return (len);
+}
+
+void _memcpy(char *src, char *dest)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+}
+
+/**
  * new_dog - Creates a new dog struct instance using given values
  * @name: The name to give the new dog struct
  * @age: The age to give the new dog struct
@@ -22,9 +51,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_d = malloc(sizeof(dog_t));
 	if (new_d != NULL)
 	{
-		new_d->name = strdup(name);
+		new_d->name = malloc(sizeof(char) * (_strlen(name) + 1));
+		if (new_d->name == NULL)
+		{
+			free(new_d);
+			return (NULL);
+		}
+		_memcpy(name, new_d->name);
 		new_d->age = age;
-		new_d->owner = strdup(owner);
+		new_d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+		if (new_d->owner == NULL)
+		{
+			free(new_d->name);
+			free(new_d);
+			return (NULL);
+		}
+		_memcpy(owner, new_d->owner);
 	}
 	return (new_d);
 }
