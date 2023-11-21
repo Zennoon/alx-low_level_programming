d8010793ca8ee25f45c5ed8671476e37df990941
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-11-21 18:34:17 +0000

    Write a function that frees the memory allocated for a list and handles loops in the list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * is_in_arr2 - Checks if a given value is an element of an array
+ * @arr: The array of pointers
+ * @n: The number of elements of the array
+ * @add: The address to check for existence in the array
+ *
+ * Return: 1 if the address exists in the pointer/address array, 0 otherwise
+ */
+int is_in_arr2(listint_t **arr, size_t n, listint_t *add)
+{
+	size_t i = 0;
+
+	while (i < n)
+	{
+		if (arr[i] == add)
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+/**
+ * free_listint_safe - Frees memory allocated for a list and handles loops
+ * @head: Head of the singly linked list
+ *
+ * Return: Size of memory freed
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	size_t size = 0, node_cnt = 0;
+	listint_t **address_arr = malloc(0);
+	listint_t *tmp;
+
+	while (*head != NULL && !is_in_arr2(address_arr, node_cnt, *head))
+	{
+		address_arr = realloc(address_arr, (node_cnt + 1)
+				      * sizeof(listint_t *));
+		if (address_arr == NULL)
+		{
+			exit(98);
+		}
+		address_arr[node_cnt] = *head;
+		size += sizeof(listint_t);
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		node_cnt++;
+	}
+	*head = NULL;
+	free(address_arr);
+	return (size);
+}
---
+++ 0x13-more_singly_linked_lists/102-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+	listint_t *head2;
+	listint_t *node;
+
+	head2 = NULL;
+	add_nodeint(&head2, 0);
+	add_nodeint(&head2, 1);
+	add_nodeint(&head2, 2);
+	add_nodeint(&head2, 3);
+	add_nodeint(&head2, 4);
+	add_nodeint(&head2, 98);
+	add_nodeint(&head2, 402);
+	add_nodeint(&head2, 1024);
+	print_listint_safe(head2);
+	head = NULL;
+	node = add_nodeint(&head, 0);
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+	add_nodeint(&head, 3);
+	add_nodeint(&head, 4);
+	node->next = add_nodeint(&head, 98);
+	add_nodeint(&head, 402);
+	add_nodeint(&head, 1024);
+	print_listint_safe(head);
+	printf("size = %lu\n %lu\n", free_listint_safe(&head), sizeof(listint_t));
+	printf("size = %lu\n", free_listint_safe(&head2));
+	printf("%p, %p\n", (void *) head2, (void *) head);
+	return (0);
+}
