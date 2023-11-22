d68041ae81640f342f6bb517e3fbe328d79f9f36
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2023-11-22 18:44:30 +0000

    Make a version of find_listint_loop which uses only one variable, but also de-attach the links (the list is destroyed)

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,25 +1,6 @@
 #include "lists.h"
 
 /**
- * count_links_between - Count the number of node links between two nodes
- * @node1: The first node
- * @node2: The second node
- *
- * Return: The number of jumps it takes to reach node2 from node1
- */
-size_t count_links_between(listint_t *node1, listint_t *node2)
-{
-	size_t distance = 0;
-
-	while (node1 != NULL && node2 != NULL && node1 != node2)
-	{
-		node1 = node1->next;
-		distance++;
-	}
-	return (distance);
-}
-
-/**
  * find_listint_loop - Finds the beginning of a loop inside a list if it exists
  * @head: The head of the linked list
  *
@@ -27,17 +8,17 @@ size_t count_links_between(listint_t *node1, listint_t *node2)
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	size_t n = 0;
-	listint_t *ptr = head;
+	listint_t *tmp;
 
-	while (ptr != NULL)
+	while (head != NULL)
 	{
-		ptr = ptr->next;
-		if (n >= count_links_between(head, ptr))
+		tmp = head->next;
+		if (tmp == head)
 		{
 			break;
 		}
-		n++;
+		head->next = head;
+		head = tmp;
 	}
-	return (ptr);
+	return (head);
 }
