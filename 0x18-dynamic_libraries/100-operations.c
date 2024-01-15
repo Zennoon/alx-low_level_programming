9208674151d62ef73ecccd0745fec9f024e18756
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-01-15 19:32:03 +0000

    Create a dynamic library whose functions can be used in a python script

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - Returns the sum of two integers
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Returns the difference between two integers
+ * @a: First Operand
+ * @b: Second operand
+ *
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Returns the product of two integers
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Returns the quotient of two integers
+ * @a: First operand (Dividend)
+ * @b: Second operand (Divisor)
+ *
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Returns the modulo of two integers
+ * @a: First operand (Dividend)
+ * @b: Second operand (Divisor)
+ *
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
