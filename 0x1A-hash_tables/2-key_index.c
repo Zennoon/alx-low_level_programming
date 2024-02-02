f178a2cf213de531531624f1506b4fca05ed2e27
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-02-02 18:28:31 +0000

    Write a function that converts the hash generated by hash_djb2 to an index to a table of given size

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,7 +7,7 @@
  * @size: The size of the hash table
  *
  * Description: This function utilizes the hash_djb2 hashing function in
- * 1-djb2.c to get the hashing of the key. This function then converts this hash
+ * 1-djb2.c to get the hashing of the key. This function converts this hash
  * to an index that can be used for the table of the given size
  * Return: An index at which to insert the key-value pair
  */
