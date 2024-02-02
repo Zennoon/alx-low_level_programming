4f0875030f184a0ac8b80e7a11a255ab4d61af17
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-02-02 19:39:30 +0000

    Write a function that inserts a new entry to a given hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (new_elem == NULL)
 		return (0);
-	if (!(*key) || ht == NULL || ht->array == NULL)
+	if (!(*key) || ht == NULL || ht->array == NULL || ht->size <= 0)
 	{
 		free(new_elem);
 		return (0);
